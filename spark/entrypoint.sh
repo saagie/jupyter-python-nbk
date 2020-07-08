c6eb1f01c76298a10b2ff8db635535c86c2501c1
Yann PETIT <yann.petit@saagie.com>
2020-07-08 14:57:15 +0200

    [jupyter-spark] remove useless namespace env var

--- spark/entrypoint.sh
+++ spark/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 chown -R jovyan /notebooks-dir
 
-TARGET_NAMESPACE="saagie1-projectc5aa8432-f94a-4707-bb9e-79e183e8b107"
 FILE_KERNEL_PY27="/home/jovyan/.local/share/jupyter/kernels/py27/kernel.json"
 FILE_KERNEL_PY36="/home/jovyan/.local/share/jupyter/kernels/py36/kernel.json"
 
